ba9e0265081669db22131fd7b3ef6633fdfc8210
jdwjdwjdw <jacobdeanwilson@gmail.com>
2020-10-18 23:51:19 -0700

    Creating Jumbotron component

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import { Inner } from './styles/jumbotron';
+import { Container, Inner, Pane, Title, SubTitle, Image } from './styles/jumbotron';
 
-export default function Jumbotron({ direction = 'row', ...restProps }) {
-	return (
-		<Inner direction={direction}>
-			<p>Hello again!</p>
-		</Inner>
-	);
+export default function Jumbotron({ children, direction = 'row', ...restProps }) {
+	return <Inner direction={direction}>{children}</Inner>;
 }
+
+Jumbotron.Container = function JumbotronContainer({ children, ...restProps }) {
+	return <Container {...restProps}>{children}</Container>;
+};
+
+Jumbotron.Pane = function JumbotronPane({ children, ...restProps }) {
+	return <Pane {...restProps}>{children}</Pane>;
+};
+
+Jumbotron.Title = function JumbotronTitle({ children, ...restProps }) {
+	return <Title {...restProps}>{children}</Title>;
+};
+
+Jumbotron.SubTitle = function JumbotronSubTitle({ children, ...restProps }) {
+	return <SubTitle {...restProps}>{children}</SubTitle>;
+};
+
+Jumbotron.Image = function Image({ ...restProps }) {
+	return <Image {...restProps} />;
+};
--- src/components/jumbotron/styles/jumbotron.js
+++ src/components/jumbotron/styles/jumbotron.js
@@ -13,3 +13,13 @@ export const Inner = styled.div`
         flex-direction: column
     }
 `;
+
+export const Pane = styled.div`width: 50%;`;
+
+export const Title = styled.h1``;
+
+export const SubTitle = styled.h2``;
+
+export const Image = styled.image``;
+
+export const Container = styled.div``;
